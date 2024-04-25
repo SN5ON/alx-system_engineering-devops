cd7c9d4d3c65a4c79c9f6a4e54790561c8c96374
SN5ON <shalomnyalila@gmail.com>
2024-04-25 04:38:56 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# install puppet-lint version
-
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.1.0',
+# Using Puppet, install flask
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
