59e918cc8caf2548151a571b16995c2bf941bce1
SN5ON <shalomnyalila@gmail.com>
2024-06-04 15:28:19 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """
- queries the Reddit API and returns the number of subscribers
+Script that queries subscribers on a given Reddit subreddit.
 """
+
 import requests
+
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
